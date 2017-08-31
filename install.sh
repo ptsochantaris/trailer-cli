14003f3e261f060bebacd30d0f07df45e99f6bf5
Paul Tsochantaris <ptsochantaris@icloud.com>
2017-09-01 00:12:51 +0200

    Simple install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+echo "*** Cleaning"
+swift package reset
+echo "*** Building"
+swift build -c release --static-swift-stdlib
+if [ $? -eq 0 ]; then
+	SRC="$(swift build -c release --show-bin-path)/trailer"
+	echo "*** Installing 'trailer' to /usr/local/bin"
+	install $SRC /usr/local/bin/trailer
+	echo "*** Done"
+else
+	echo
+	echo "*** Build failed, ensure you are using Swift 4.x on the command line"
+fi
