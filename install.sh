85b7f9650a6af8d98ff42f9538b52fa593b1335b
Paul Tsochantaris <ptsochantaris@icloud.com>
2017-09-01 20:34:54 +0100

    Sudo for install in Linux

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ echo "*** Building"
 swift build -c release --static-swift-stdlib
 if [ $? -eq 0 ]; then
 	SRC="$(swift build -c release --show-bin-path)/trailer"
-	echo "*** Installing 'trailer' to /usr/local/bin"
-	install $SRC /usr/local/bin/trailer
+	echo "*** Installing 'trailer' to /usr/local/bin, please enter your sudo password if needed"
+	sudo install $SRC /usr/local/bin/trailer
 	echo "*** Done"
 else
 	echo
