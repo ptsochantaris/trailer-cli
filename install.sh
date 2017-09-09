4365c1b2331a76aa40620dbac7b8e8156d07be08
Paul Tsochantaris <ptsochantaris@icloud.com>
2017-09-09 23:21:52 +0100

    Cleanup after install

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@ if [ $? -eq 0 ]; then
 	SRC="$(swift build -c release --show-bin-path)/trailer"
 	echo "*** Installing 'trailer' to /usr/local/bin, please enter your sudo password if needed"
 	sudo install $SRC /usr/local/bin/trailer
+	echo "*** Cleaning Up"
+	swift package reset
 	echo "*** Done"
 else
 	echo
