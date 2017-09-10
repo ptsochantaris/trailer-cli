8689099d9e364ce678359ea34fe16dfed26b12c3
Paul Tsochantaris <ptsochantaris@icloud.com>
2017-09-10 01:02:44 +0100

    Command line compilation improvements: Optimisations and static linking

--- install.sh
+++ install.sh
@@ -2,9 +2,11 @@
 echo "*** Cleaning"
 swift package reset
 echo "*** Building"
-swift build -c release --static-swift-stdlib
+swift build -c release -Xswiftc -static-stdlib -Xswiftc -Ounchecked -Xswiftc -whole-module-optimization
 if [ $? -eq 0 ]; then
 	SRC="$(swift build -c release --show-bin-path)/trailer"
+	echo "*** Stripping symbols"
+	strip $SRC
 	echo "*** Installing 'trailer' to /usr/local/bin, please enter your sudo password if needed"
 	sudo install $SRC /usr/local/bin/trailer
 	echo "*** Cleaning Up"
