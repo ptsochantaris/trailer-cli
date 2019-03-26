a68ea8937ab550ce46b36ae2b513701ce58305c8
Paul Tsochantaris <ptsochantaris@icloud.com>
2019-03-26 18:55:39 +0000

    Version bump

--- Sources/trailer/Core/Config.swift
+++ Sources/trailer/Core/Config.swift
@@ -17,7 +17,7 @@ struct Config {
 
 	let versionMajor = 1
 	let versionMinor = 0
-	let versionPatch = 8
+	let versionPatch = 9
 	var versionString: String {
 		return [versionMajor, versionMinor, versionPatch].map { String($0) }.joined(separator: ".")
 	}
