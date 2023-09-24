15b3d76e9d3858a68df2e5ed850a6a022c00efb4
Paul Tsochantaris <ptsochantaris@icloud.com>
2023-09-24 12:27:58 +0100

    Non-Apple network fix

--- Sources/trailer/API/Network.swift
+++ Sources/trailer/API/Network.swift
@@ -33,6 +33,18 @@ enum Network {
         defer {
             networkGate.returnTicket()
         }
+        #if canImport(FoundationNetworking)
+        return try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<Data, Error>) in
+            _ = urlSession.dataTask(with: req) { data, _, error in
+                if let error {
+                    continuation.resume(throwing: error)
+                } else {
+                    continuation.resume(returning: data ?? Data())
+                }
+            }
+        }
+        #else
         return try await urlSession.data(for: req).0
+        #endif
     }
 }
